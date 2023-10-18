b2630196292a4ef9781c671714359402ec61b9f7
creyes25 <carolina.reyes2022@gmail.com>
2023-10-17 21:20:05 -0500

    add feature to create a new element and added it to corresponding column (odd or even)

--- srcipt.js
+++ srcipt.js
@@ -1,5 +1,11 @@
 const numCont = document.querySelector('.counter')
 const numDisplay = document.querySelector('.num')
+const oddDiv = document.querySelector('.odd')
+const evenDiv = document.querySelector('.even')
+const divisible3 = document.querySelector('.divisible3')
+const divisible5 = document.querySelector('.divisible5')
+const divisible15 = document.querySelector('.divisible15')
+
 
 let counter = 0
 
@@ -7,6 +13,28 @@ let counter = 0
 function increaseCounter () {
   counter++
   numDisplay.innerHTML = counter
+
+  if (counter % 2 != 0) {
+    createDiv(oddDiv)
+  }
+
+  if (counter % 2 === 0) {
+    createDiv(evenDiv)
+  }
+}
+
+function createDiv(appendTo) {
+  const newDiv = document.createElement('div')
+  newDiv.className = 'new-div flex align-items'
+  const newDivCont = document.querySelector('.new-div')
+  const newH2 = document.createElement('h2')
+  newH2.className = 'new-num'
+  
+  newDiv.appendChild(newH2)
+
+  appendTo.appendChild(newDiv)
+  newH2.innerHTML = counter
+
 }
 
 
